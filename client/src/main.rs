68fd7da438e2bd2655d28c8fb6fe0599a780b552
waylandc <wayland.chan@gmail.com>
2020-08-20 00:34:36 +0800

    handshake
    perform protocol handshake when client connects to server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,24 +1,27 @@
 use myapi::codec::MyCodec;
 use std::error::Error;
+use std::str::from_utf8;
 use std::{thread, time};
-use tokio::net::tcp::OwnedReadHalf;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::net::TcpStream;
 use tokio::stream::StreamExt;
 use tokio_util::codec::FramedRead;
+
+static CLIENT_VERSION: &'static str = "1.0";
 
 #[tokio::main]
 async fn main() {
     let stream = TcpStream::connect("0.0.0.0:8080").await.unwrap();
     println!("Connected to 0.0.0.0:8080");
 
-    let (rx, _tx) = stream.into_split();
+    let (rx, tx) = stream.into_split();
 
     tokio::spawn(async move {
-        if let Err(e) = listener_thread(rx).await {
+        if let Err(e) = do_handshake(tx, rx).await {
             panic!("error occurred {}", e);
         }
     });
-
     //TODO if we dont sleep here, execution finishes before
     //spawned thread has time to do anything
     //eventually we will spin another thread to take user input and send
@@ -26,14 +29,50 @@ async fn main() {
     thread::sleep(time::Duration::from_secs(10));
 }
 
-async fn listener_thread(stream: OwnedReadHalf) -> Result<(), Box<dyn Error>> {
-    let mut reader = FramedRead::new(stream, MyCodec::new());
+//do_handshake sends the client version to the server to initialize
+//and verify the protocol version. An Error is returned if the current
+//version is not supported
+async fn do_handshake(
+    mut snd: OwnedWriteHalf,
+    mut recv: OwnedReadHalf,
+) -> Result<String, Box<dyn Error>> {
+    //to handshake with server, we sent our client version
+    //immediately after connecting
+    let buf: Vec<u8> = CLIENT_VERSION.as_bytes().to_vec();
+    snd.write_all(&buf).await.unwrap();
 
-    while let message = reader.next().await {
-        match message {
-            Some(msg) => println!("received: {:?}", msg),
-            None => continue,
+    let mut in_buf: [u8; 256] = [0; 256];
+    let n = recv
+        .read(&mut in_buf)
+        .await
+        .expect("Failed to receive handshake");
+
+    if n == 0 {
+        return Err("server failed to return handshake".into());
+    } else {
+        let server_protocol = from_utf8(&in_buf).unwrap().to_string();
+
+        println!("server is running protocol version {}", server_protocol);
+        let ver: String = CLIENT_VERSION.into();
+        //TODO enumerate our version somewhere
+        //Check that our protocol version matches server's
+        match server_protocol {
+            ver => println!(
+                "Handshake successful, using protocol version {}",
+                server_protocol
+            ),
+            _ => return Err("Client version unsupported".into()),
         }
+
+        // Handshake successful, proceed
+        let mut reader = FramedRead::new(recv, MyCodec::new());
+
+        while let Some(message) = reader.next().await {
+            match message {
+                Ok(msg) => println!("received: {:?}", msg),
+                Err(_) => continue,
+            }
+        }
+        Ok("OK".to_string())
     }
-    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,26 +1,60 @@
 use myapi::app_protocol::*;
 
 use protobuf::Message;
-use std::error::Error as StdError;
-use tokio::io::AsyncWriteExt;
+use std::error::Error;
+use std::str::from_utf8;
+use std::string;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::net::{TcpListener, TcpStream};
 
+static PROTOCOL_VERSION: &'static str = "1.0";
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn StdError>> {
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut listener = TcpListener::bind("0.0.0.0:8080").await?;
     println!("Server bound and listening on 0.0.0.0:8080");
 
     loop {
         let (stream, addr) = listener.accept().await?;
 
+        let (rx, tx) = stream.into_split();
         println!("client connected from {}", addr);
         tokio::spawn(async move {
-            process(stream).await;
+            match process(tx, rx).await.unwrap() {
+                () => println!("process returned OK"),
+                _ => println!("process returned error"),
+            }
         });
     }
 }
 
-async fn process(mut stream: TcpStream) {
+async fn process(mut snd: OwnedWriteHalf, mut recv: OwnedReadHalf) -> Result<(), Box<dyn Error>> {
+    //perform protocol version handshake with client
+    let mut in_buf: [u8; 1024] = [0; 1024];
+    let n = recv
+        .read(&mut in_buf)
+        .await
+        .expect("Error reading handshake");
+
+    if n == 0 {
+        return Err("client failed handshake".into());
+    }
+
+    let client_version = from_utf8(&in_buf).unwrap().to_string();
+    let ver: String = PROTOCOL_VERSION.into();
+
+    match client_version {
+        ver => {
+            println!("client version OK");
+            snd.write_all(PROTOCOL_VERSION.as_bytes()).await.unwrap();
+        }
+        _ => {
+            snd.write_all(b"UNSUPPORTED_VERSION").await.unwrap();
+            return Err("Client is using unsupported version".into());
+        }
+    };
+
     //TODO just hardcoding a pb to send across wire for now
     // We will just encode a protobuf message and then send to client
     let mut lr = LoginResponse::default();
@@ -33,6 +67,7 @@ async fn process(mut stream: TcpStream) {
 
     let buf: Vec<u8> = wrapper.write_to_bytes().unwrap();
 
-    stream.write_all(&buf).await.unwrap();
+    snd.write_all(&buf).await.unwrap();
     println!("Sent {:?}", lr);
+    Ok(())
 }
